b6979028442e90ee2420186faab8c97c79ab6071
Favour okerri <favourokerri767@gmail.com>
2024-03-07 08:53:01 +0100

    wrote test for dupicate email registration

--- Application/users/tests.py
+++ Application/users/tests.py
@@ -1,4 +1,6 @@
 from django.test import TestCase, Client
+from django.db import transaction
+from django.db import IntegrityError
 from django.urls import reverse
 from django.contrib.auth.models import User
 # Create your tests here.
@@ -17,3 +19,18 @@ class SignUpViewTestCase(TestCase):
         response = self.client.post(self.signup_url, data)
         self.assertEqual(response.status_code, 302) #check if redirection occured
         self.assertTrue(User.objects.filter(username='favour@gmail.com').exists())
+    
+    def test_DuplicateUser(self):
+        User.objects.create(first_name='peter', email='john@gmail.com', username='john@gmail.com', password='12345678')
+        data = {
+            'firstName': 'john',
+            'email': 'john@gmail.com',
+            'password': '12345678'
+        }
+        try:
+            with transaction.atomic():
+                response = self.client.post(self.signup_url, data)
+        except IntegrityError:
+            self.fail('user with thie email alredy exits')
+        self.assertEqual(response.status_code, 200) #check that there was no redirect
+        self.assertFalse(User.objects.filter(first_name='john').exists())
